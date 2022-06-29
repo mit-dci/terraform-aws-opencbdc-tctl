7aa13c54f058831e50d7fe164ecf900887956d5d
Benjamin Kincaid <ben.kincaid@rocketinsights.com>
2022-06-29 15:41:54 -0400

    add encryption
    Signed-off-by: Kevin Karwaski <kevin.karwaski@gmail.com>

--- modules/test-controller/main.tf
+++ modules/test-controller/main.tf
@@ -823,6 +823,14 @@ resource "aws_opensearch_domain" "this" {
   domain_name    = "${var.environment}-testresults"
   engine_version = var.opensearch_engine_version
 
+  encrypt_at_rest {
+    enabled = true
+  }
+
+  domain_endpoint_options {
+    enforce_https = true
+  }
+
   cluster_config {
     instance_type  = var.opensearch_instance_type
     instance_count = var.opensearch_instance_count
