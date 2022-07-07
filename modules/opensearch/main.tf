9614a11dd26125f005acb773a2d9b70fabaf513a
Benjamin Kincaid <ben.kincaid@rocketinsights.com>
2022-07-07 13:20:38 -0400

    tls security policy

--- modules/opensearch/main.tf
+++ modules/opensearch/main.tf
@@ -20,6 +20,7 @@ resource "aws_opensearch_domain" "this" {
 
   domain_endpoint_options {
     enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
   cluster_config {
