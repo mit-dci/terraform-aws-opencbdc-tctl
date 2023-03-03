e8bbe7dc4d0e5a0fa898c1afab1bd9c242f94c2e
Jayson Sizer McIntosh <jaysosmc@amazon.com>
2023-03-03 09:30:37 -0500

    fixing broken reference to ubuntu ami
    Signed-off-by: Jayson Sizer McIntosh <jaysosmc@amazon.com>

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -61,12 +61,21 @@ resource "aws_key_pair" "bastion" {
 
 # Lookup ubuntu AMI ID
 data "aws_ami" "bastion" {
+
+  most_recent = true
+
   owners = ["099720109477"] # Canonical (Ubuntu)
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20210129"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
 }
 
 # Elastic IP to assign to bastion host
